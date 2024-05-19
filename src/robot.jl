48d3fde8a45a1b33011c5a11fe02b7e75e95fe56
Chrisi Zeh <chrisizeh@cronos.local>
2024-05-19 17:47:18 +0200

    add comments for robot struct

--- src/robot.jl
+++ src/robot.jl
@@ -2,19 +2,20 @@ include("area.jl")
 
 using ElasticArrays
 
-mutable struct Robot
-    #=
-    Structure for storing all necessary variable for a round robot.
+"""
+   Robot
 
-    :id: unique value to identify robot
-    :radius: the radius of the robot in meter
-    :history: all previous positions since instantiation
-    :pos_x: current center point of robot on x-axis
-    :pos_y: current center point of robot on y-axis
-    :deg: current degree of robot in rad, x-axis equals 0 degrees.
-    :vel_left: current velocity of left tire, measured in meter per seconds.
-    :vel_right: current velocity of right tire, measured in meter per seconds.
-    =#
+   Structure for storing all necessary variable for a round robot.
+
+# Fields
+- `id::Int32`: unique value to identify robot
+- `radius::Float32`: the radius of the robot in meter
+- `history::ElasticArray{Float64}`: all previous positions since instantiation
+- `pos::Array{Float64}`: current center point of robot
+- `deg::Float32`: current degree of robot in rad, x-axis equals 0 degrees.
+- `vel::Array{Float32}`: current velocity of left tire, measured in meter per seconds.
+"""
+mutable struct Robot
 
 	id::Int32
 	radius::Float32      # m
@@ -33,6 +34,17 @@ end
 
 Base.show(io::IO, rob::Robot) = print(io, "{id: ", rob.id, " pos: ", rob.pos, "}")
 
+
+"""
+update_speed!(robot::Robot, left::Float64, right::Float64) -> None
+
+Update the Speed of the robot
+
+# Arguments
+- `robot::Robot`: Robot to update the value from
+- `left::Float64`: Speed to set for the left tire in m/s
+- `right::Float64`: Speed to set for the right tire in m/s
+"""
 function update_speed!(robot::Robot, left::Float64, right::Float64)
 	robot.vel = [left, right]
 end
@@ -41,12 +53,33 @@ function update_speed!(robot::Robot, left::Int64, right::Int64)
 	robot.vel = [left * 1.0, right * 1.0]
 end
 
+
+"""
+stop(robot::Robot)
+
+Reset the speed of both tires to 0.
+
+# Arguments
+- `robot::Robot`: Robot to update the value from
+"""
 function stop(robot::Robot)
 	robot.update_speed(0, 0)
 end
 
 
-# Paper for calculation: https://rossum.sourceforge.net/papers/DiffSteer/
+"""
+move!(robot::Robot, sec::Float64, checkBorder::Bool, border::Border)
+
+Move the robot for a specified amount of time. If checkBorder is False, the robot can exit the area.
+	The movement with different velocities for both tires is calculated using the paper
+	https://rossum.sourceforge.net/papers/DiffSteer/.
+
+# Arguments
+- `robot::Robot`: Robot to update the value from
+- `sec::Float64`: Timeframe to move the robot
+- `checkBorder::Bool`: Flag, indicating if the border can be crossed
+- `border::Border`: Border of movement area
+"""
 function move!(robot::Robot, sec::Float64, checkBorder::Bool, border::Border)
 	append!(robot.history, [robot.pos[1], robot.pos[2], robot.deg])
 
@@ -76,6 +109,20 @@ function move!(robot::Robot, sec::Float64, checkBorder::Bool, border::Border)
 end
 
 
+"""
+check_border(robot::Robot, border::Border, new_pos::Array{Float64})
+
+To prevent crossing the border, intersections are solved by repositioning the robot
+	in the corresponding axis directly on the border, pos = border - robot radius.
+
+# Arguments
+- `robot::Robot`: Robot to check for border intersections
+- `border::Border`: Border to check for intersection
+- `new_pos::Array{Float64}`: Current position of robot after movement update
+
+# Returns
+- `Array{Float64}`: New position of robot after fixing intersections
+"""
 function check_border(robot::Robot, border::Border, new_pos::Array{Float64})
 	if (new_pos[1] + robot.radius > border.right)
 		new_pos[1] = border.right - robot.radius
@@ -93,20 +140,43 @@ function check_border(robot::Robot, border::Border, new_pos::Array{Float64})
 end
 
 
+"""
+move_intersection!(robot::Robot, robots::Array{Robot}=[])
+
+Robots cannot move on top of each other. For each robot, the distant to each other is checked.
+	If the distance iis less than the radii combined, robot is moved.
+
+# Arguments
+- `robot::Robot`: Robot to move to prevent intersection
+- `robots::Array{Robot}`: List of robots to check for intersection
+"""
 function move_intersection!(robot::Robot, robots::Array{Robot}=[])
     for other in robots
         if(robot.id != other.id)
 			dist = sqrt(abs(other.pos[1] - robot.pos[1])^2 + abs(other.pos[2] - robot.pos[2])^2)
 
 			if(dist < robot.radius + other.radius)
-				fix_intersection(robot, other)
+				robot.pos = fix_intersection(robot, other)
 			end
         end
     end
 end
 
 
-function fix_intersection(robot::Robot, other_robot::Robot)
+"""
+check_intersection(robot::Robot, other_robot::Robot)
+
+To solve the intersection, the robot is moved away from the other robot on the vector
+	combining both center points.
+
+# Arguments
+- `robot::Robot`: Robot to move to prevent intersection
+- `other_robot::Robot`: Other Robot, which is part of the intersection
+
+# Returns
+- `Array{Float64}`: New position of robot after fixing intersection
+"""
+function check_intersection(robot::Robot, other_robot::Robot)
 	x = other_robot.pos[1] - robot.pos[1]
 	y = other_robot.pos[2] - robot.pos[2]
 
@@ -119,6 +189,9 @@ function fix_intersection(robot::Robot, other_robot::Robot)
 		deg = -acos(x / dist)
 	end
 
-	robot.pos[1] = robot.pos[1] - cos(deg) * (r - dist)
-	robot.pos[2] = robot.pos[2] - sin(deg) * (r - dist)
+	new_pos = []
+	new_pos[1] = robot.pos[1] - cos(deg) * (r - dist)
+	new_pos[2] = robot.pos[2] - sin(deg) * (r - dist)
+
+	return new_pos
 end
