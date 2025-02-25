63b1819bbbf2915a78efe88706b61b5cb133a8d4
chrisizeh <me@christinezeh.com>
2025-02-25 23:07:30 +0100

    add combined algo and readme

--- README.md
+++ README.md
@@ -6,11 +6,32 @@ This repository hosts a simulation of an agent-based model featuring mobile robo
 
 * **Realistic Movement:** Robots move in a physically accurate manner. Collisions and interactions with the environment borders are handled according to physics principles.
 * **Environment Boundaries:** The simulation environment is bounded by borders.
+* Obstacles can be placed in the bounded environment.
+* Sensoric Data: Robots are equipt with infrared sensors in each direction to get information of nearby obstacles or robots
+
 
 ## Future Features
 
-* Obstacles can be placed in the bounded environment.
-* Sensoric Data: Robots are equipt with infrared sensors in each direction to get information of nearby obstacles or robots
 * Improved collision handling of multiple robots
 * Every Robot has a heat camera to find the target in a search and rescue szenario
 * Nearby robots can exchange information
+
+# Swarm Behaviour
+
+For this robot simulation two swarming algorithms are implemented.
+
+## Tracking
+
+Currently the algorithm is single target, but will be extended to track multiple targets of a swarm.
+
+![Tracking Algorithm](results/tracking.gif)
+
+## Flocking
+
+![Flocking Algorithm](results/flocking.gif)
+
+## Search and Rescue
+
+To allow for search and rescue missions in environment without long range communication, these algorithms are combined. This allows for fast transmission of the target location, without retracing the traveled path.
+
+![Flocking + Tracking Algorithm](results/combined_algorithms.gif)
--- src/path_building.jl
+++ src/path_building.jl
@@ -1,16 +1,17 @@
 include("include_me.jl")
 
 obstacles = []
-push!(obstacles, Round_Obstacle([225.0, 175.0], 225.0, color="#f19066"))
-push!(obstacles, Round_Obstacle([975.0, 175.0], 275.0, color="#f19066"))
-push!(obstacles, Round_Obstacle([1500.0, 300.0], 500.0, color="#f19066"))
-push!(obstacles, Round_Obstacle([750.0, 720.0], 100.0, color="#f19066"))
-push!(obstacles, Round_Obstacle([1300.0, 1300.0], 300.0, color="#f19066"))
-push!(obstacles, Round_Obstacle([350.0, 900.0], 75.0, color="#f19066"))
-push!(obstacles, Round_Obstacle([0.0, 500.0], 230.0, color="#f19066"))
-push!(obstacles, Round_Obstacle([100.0, 1200.0], 50.0, color="#26de81"))
+push!(obstacles, Round_Obstacle([225.0, 175.0], 225.0, color="#465361"))
+push!(obstacles, Round_Obstacle([975.0, 175.0], 275.0, color="#465361"))
+push!(obstacles, Round_Obstacle([1500.0, 300.0], 500.0, color="#465361"))
+push!(obstacles, Round_Obstacle([750.0, 720.0], 100.0, color="#465361"))
+push!(obstacles, Round_Obstacle([1300.0, 1300.0], 300.0, color="#465361"))
+push!(obstacles, Round_Obstacle([350.0, 900.0], 75.0, color="#465361"))
+push!(obstacles, Round_Obstacle([0.0, 500.0], 230.0, color="#465361"))
 
-area = Area(0, 1300, 0, 1300; obstacles=obstacles)
+goal = Round_Obstacle([100.0, 1200.0], 50.0, color="#2C68A7")
+
+area = Area(0, 1300, 0, 1300; obstacles=obstacles, goal=goal)
 
 vMin = 50.0
 # vMin = 10.0
@@ -19,11 +20,11 @@ num_robots = 7*4
 for lines in 2:5
     for i in 1:7
         robot = Robot(i; radius=10, 
-            color="#2ecc71",
+            color="#E08631",
             vel=[0, 0],
             pos=[500 + 180 * (lines % 2) + (-1)^lines * 30 * (i - 1), 110 - 30 * (lines - 2)], 
             deg = pi/2,
-            sensor_dist=100.0, sensor_num=6, spec=2*pi)
+            sensor_dist=100.0, sensor_num=8, spec=2*pi, sensor_deg=pi/2)
         push!(robots, robot)
     end
 end
@@ -31,16 +32,17 @@ end
 
 update_speed!(robots[1], vMin, vMin)
 
-sim = Simulation(robots, area; open_area=true, num_grid=20, time_step=0.1)
-centerSpeed = 0.6
+sim = Simulation(robots, area; open_area=true, num_grid=20, time_step=0.2)
+centerSpeed = 0.5
 turnSpeed = 0.8
 
 vals = [0., 0.]
 
-for j in 1:1000
+for j in 1:200
     update!(sim)
 
     dist = get_sensoric_data(robots[1], robots[2])
+    println(dist)
     if (minimum(dist) > 98)
         update_speed!(robots[1], 0, 0)
     else
@@ -48,7 +50,6 @@ for j in 1:1000
     end
 
     for i in 2:num_robots-1
-
         dist1 = get_sensoric_data(robots[i], robots[i-1])
         dist2 = get_sensoric_data(robots[i], robots[i+1])
 
@@ -73,4 +74,4 @@ for j in 1:1000
     end
 end
 
-plot_hist(sim; speedup=1.0, showSensor=false)
+plot_hist(sim; speedup=5.0, showSensor=false)
